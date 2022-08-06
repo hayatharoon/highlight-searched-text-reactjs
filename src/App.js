92f0c451218b448ec266e534baf1561886efb2df
Haroon Hayat <hayatharoon@yahoo.com>
2022-08-07 00:10:45 +0500

    changing in App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const initialText =
     'React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page, mobile, or server-rendered applications with frameworks like Next.js. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality';
-  const [text, setText] = useState(initialText);
-  const [inputValue, setInputValue] = useState('');
+  const text = useState(initialText);
 
   function handleChange(event) {
     let value = event.target.value;
@@ -16,10 +15,12 @@ function App() {
     <div className='App'>
       <div className='card'>
         <input type='text' placeholder='Enter text to search' onChange={handleChange} />
-        <p id='myText'>{text}</p>
+        <p id='myText'>{text[0]}</p>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
